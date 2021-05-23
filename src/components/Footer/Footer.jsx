4fccfef690d836c325435dd07d96973194f755b6
Jennifer Goijman <jennifer.goijman@bourne-leisure.co.uk>
2021-05-23 22:16:38 +0200

    migrate Footer to styled-components

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { FooterStyled, FooterTitleStyled, FooterCopyStyled} from './Footer.styled';
+
+export const Footer = () => (
+  <FooterStyled>
+    <FooterTitleStyled>PlatziConf Merch</FooterTitleStyled>
+    <FooterCopyStyled>Todos los derechos reservados</FooterCopyStyled>
+  </FooterStyled>
+);
---
+++ src/components/Footer/Footer.styled.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const FooterStyled = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const FooterTitleStyled = styled.p``;
+
+export const FooterCopyStyled = styled.p``;
