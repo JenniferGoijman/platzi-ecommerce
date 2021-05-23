5bad569773a484715098bcea77cdaa0028f171fa
Jennifer Goijman <jennifer.goijman@bourne-leisure.co.uk>
2021-05-23 17:34:41 +0200

    create hook useGoogleAddress

--- src/containers/Success.jsx
+++ src/containers/Success.jsx
@@ -1,19 +1,21 @@
 import React, { useContext } from 'react';
 import AppContext from '../context/AppContext';
 import Map from '../components/Map';
+import useGoogleAddress from '../hooks/useGoogleAddress'
 import '../styles/components/Success.css';
 
 const Success = () => {
   const { state } = useContext(AppContext);
   const { buyer } = state;
+  const location = useGoogleAddress(buyer.address)
 
   return (
-    <div className="Succes">
+    <div className="Success">
       <div className="Success-content">
         <h2>{`${buyer.name}, gracias por tu compra`}</h2>
         <span>Tu pedido llegará en 3 días a tu dirección:</span>
         <div className="Success-map">
-          <Map />
+          <Map data={location} />
         </div>
       </div>
     </div>
